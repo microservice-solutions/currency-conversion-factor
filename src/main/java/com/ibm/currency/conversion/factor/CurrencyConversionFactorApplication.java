02cb9ea930762147067fd223f6f9477bc9a28834
Probal Basak <probasak@in.ibm.com>
2021-01-27 16:43:51 +0530

    Added eureka client support

--- src/main/java/com/ibm/currency/conversion/factor/CurrencyConversionFactorApplication.java
+++ src/main/java/com/ibm/currency/conversion/factor/CurrencyConversionFactorApplication.java
@@ -2,8 +2,10 @@ package com.ibm.currency.conversion.factor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyConversionFactorApplication {
 
 	public static void main(String[] args) {
